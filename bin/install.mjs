7daeb388090fddeeac795775e87f84d0aadc3eec
zdm <zdm@softvisio.net>
2023-07-11 04:39:51 +0300

    fix: resources

--- bin/install.mjs
+++ bin/install.mjs
@@ -2,11 +2,11 @@
 
 import externalResources from "@softvisio/external-resources";
 
-externalResources.add( `softvisio-node/uws/resources/node-v${process.versions.modules}-${process.platform}-${process.arch}.node`, import.meta.url, { "location": "lob/binaries" } );
+externalResources.add( `softvisio-node/uws/resources/node-v${process.versions.modules}-${process.platform}-${process.arch}.node`, import.meta.url, { "location": "lib/binaries" } );
 
 // under windows download linux binaries for vmware
 if ( process.platform === "win32" ) {
-    externalResources.add( `softvisio-node/uws/resources/node-v${process.versions.modules}-linux-${process.arch}.node`, import.meta.url, { "location": "lob/binaries" } );
+    externalResources.add( `softvisio-node/uws/resources/node-v${process.versions.modules}-linux-${process.arch}.node`, import.meta.url, { "location": "lib/binaries" } );
 }
 
 const res = await externalResources.update( {
