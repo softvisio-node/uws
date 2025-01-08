d19d032fe034141ed731307673a1a4024f7c2abb
zdm <zdm@softvisio.net>
2025-01-08 21:03:54 +0200

    chore: fix external resource id

--- bin/install.js
+++ bin/install.js
@@ -5,6 +5,7 @@ import externalResources from "@softvisio/utils/external-resources";
 externalResources.add( {
     "id": "softvisio-node/uws",
     "node": true,
+    "caller": import.meta.url,
 } );
 
 const res = await externalResources.install( {
